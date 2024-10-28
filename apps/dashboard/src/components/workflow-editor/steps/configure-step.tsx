bf674732182f08bdd9cb86b4148f5ea9f11f5920
Biswajeet Das <biswajeetdas18@gmail.com>
2024-10-28 21:36:55 +0530

    feat: banner

--- apps/dashboard/src/components/workflow-editor/steps/configure-step.tsx
+++ apps/dashboard/src/components/workflow-editor/steps/configure-step.tsx
@@ -4,18 +4,43 @@ import { InApp } from './in-app';
 import { Separator } from '@/components/primitives/separator';
 import { RiArrowLeftSLine, RiCloseFill } from 'react-icons/ri';
 import { Button } from '@/components/primitives/button';
+import { Link, useParams } from 'react-router-dom';
+import { useEnvironment } from '@/context/environment/hooks';
+import { buildRoute, ROUTES } from '@/utils/routes';
+import Chat from './chat';
 
 export function ConfigureStep() {
+  const { currentEnvironment } = useEnvironment();
+  const { workflowId = '' } = useParams<{
+    workflowId: string;
+  }>();
+
   return (
     <aside className="text-foreground-950 flex h-full w-[300px] max-w-[350px] flex-col border-l pb-5 pt-3.5 [&_input]:text-xs [&_input]:text-neutral-600 [&_label]:text-xs [&_label]:font-medium [&_textarea]:text-xs [&_textarea]:text-neutral-600">
       <div className="flex items-center gap-2.5 px-3 pb-3.5 text-sm font-medium">
-        <Button variant="link" size="icon" className="size-4" type="button">
-          <RiArrowLeftSLine />
-        </Button>
+        <Link
+          to={buildRoute(ROUTES.EDIT_WORKFLOW, {
+            environmentId: currentEnvironment?._id ?? '',
+            workflowId,
+          })}
+          className="flex items-center"
+        >
+          <Button variant="link" size="icon" className="size-4" type="button">
+            <RiArrowLeftSLine />
+          </Button>
+        </Link>
         <span>Configure Step</span>
-        <Button variant="link" size="icon" className="ml-auto size-4" type="button">
-          <RiCloseFill />
-        </Button>
+        <Link
+          to={buildRoute(ROUTES.EDIT_WORKFLOW, {
+            environmentId: currentEnvironment?._id ?? '',
+            workflowId,
+          })}
+          className="ml-auto flex items-center"
+        >
+          <Button variant="link" size="icon" className="size-4" type="button">
+            <RiCloseFill />
+          </Button>
+        </Link>
       </div>
       <Separator />
       <Step />
@@ -29,31 +54,11 @@ const Step = () => {
     case StepTypeEnum.IN_APP:
       return <InApp />;
 
-    case StepTypeEnum.EMAIL:
-      return 'Email';
-
-    case StepTypeEnum.TRIGGER:
-      return <>TRIGGER</>;
-
-    case StepTypeEnum.SMS:
-      return 'sms';
-
-    case StepTypeEnum.CHAT:
-      return 'chat';
-
-    case StepTypeEnum.PUSH:
-      return 'Push';
-
-    case StepTypeEnum.DELAY:
-      return 'Delay';
-
-    case StepTypeEnum.DIGEST:
-      return 'Digest';
-
-    case StepTypeEnum.CUSTOM:
-      return 'Custom';
-
+    /**
+     * TODO: Add other step types here
+     * For now, it is just a placeholder with the use sdk banner
+     */
     default:
-      return <>Unknown Step</>;
+      return <Chat />;
   }
 };
--- apps/dashboard/src/components/workflow-editor/steps/in-app.tsx
+++ apps/dashboard/src/components/workflow-editor/steps/in-app.tsx
@@ -1,51 +1,14 @@
 import { RiArrowRightSLine, RiPencilRuler2Fill } from 'react-icons/ri';
 import { Button } from '../../primitives/button';
-import { CopyButton } from '../../primitives/copy-button';
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '../../primitives/form/form';
-import { Input, InputField } from '../../primitives/input';
 import { Separator } from '../../primitives/separator';
-import { useStep } from './use-step';
+import { CommonFields } from './common-fields';
+import { DeleteStepButton } from './delete-step-button';
 
 export function InApp() {
-  const { stepIndex, control } = useStep();
-
   return (
-    <>
+    <div className="flex flex-1 flex-col">
       <div className="flex flex-col gap-4 p-3">
-        <FormField
-          control={control}
-          name={`steps.${stepIndex}.name`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Step Name</FormLabel>
-              <FormControl>
-                <InputField>
-                  <Input placeholder="Untitled" {...field} />
-                </InputField>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={control}
-          name={`steps.${stepIndex}.stepId`}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Step Identifier</FormLabel>
-              <FormControl>
-                <InputField className="flex overflow-hidden pr-0">
-                  <Input placeholder="Untitled" {...field} />
-                  <CopyButton
-                    content={field.value}
-                    className="rounded-md rounded-s-none border-b-0 border-r-0 border-t-0 text-neutral-400"
-                  />
-                </InputField>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <CommonFields />
       </div>
       <Separator />
       <div className="px-3 py-4">
@@ -54,6 +17,8 @@ export function InApp() {
           Configure in-app template <RiArrowRightSLine className="ml-auto h-4 w-4 text-neutral-600" />
         </Button>
       </div>
-    </>
+
+      <DeleteStepButton />
+    </div>
   );
 }
