a7206c82c861c79cc4f9ffd37c1f79e421408f26
Pawan Jain <jainpawan211199@gmail.com>
2024-11-05 20:39:25 +0530

    fix: prod staging env value

--- apps/web/src/components/layout/components/v2/HeaderNav.tsx
+++ apps/web/src/components/layout/components/v2/HeaderNav.tsx
@@ -59,7 +59,7 @@ export function HeaderNav() {
 
           {/* Ugly fallback to satisfy the restrictive typings of the NotificationCenterWidget */}
 
-          {IS_NOVU_PROD_STAGING || true ? (
+          {IS_NOVU_PROD_STAGING ? (
             <Dropdown
               control={
                 <ActionIcon variant="transparent">
