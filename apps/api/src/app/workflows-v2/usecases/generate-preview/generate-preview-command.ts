b37de00de329fa49f1a950e0e3a8ef48eb9e238a
Gosha <djabarov.george@gmail.com>
2024-11-06 13:06:45 +0200

    fix(api): import

--- apps/api/src/app/workflows-v2/usecases/generate-preview/generate-preview-command.ts
+++ apps/api/src/app/workflows-v2/usecases/generate-preview/generate-preview-command.ts
@@ -1,5 +1,5 @@
 import { EnvironmentWithUserObjectCommand } from '@novu/application-generic';
-import { GeneratePreviewRequestDto } from '@novu/shared/dist/cjs/dto/workflows/generate-preview-request.dto';
+import { GeneratePreviewRequestDto } from '@novu/shared';
 
 export class GeneratePreviewCommand extends EnvironmentWithUserObjectCommand {
   workflowId: string;
