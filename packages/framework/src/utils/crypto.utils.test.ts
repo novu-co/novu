924248075f06da6dcd7aece1860a7e0a3fbce9a0
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-11-08 00:51:33 +0100

    test: organize crypto utils test structure

--- packages/framework/src/utils/crypto.utils.test.ts
+++ packages/framework/src/utils/crypto.utils.test.ts
@@ -2,15 +2,17 @@ import { createHmac } from 'node:crypto';
 import { describe, expect, it } from 'vitest';
 import { createHmacSubtle } from './crypto.utils';
 
-const createHmacNode = (secretKey: string, data: string): string => {
-  return createHmac('sha256', secretKey).update(data).digest('hex');
-};
+describe('crypto utils', () => {
+  describe('createHmacSubtle', () => {
+    const createHmacNode = (secretKey: string, data: string): string => {
+      return createHmac('sha256', secretKey).update(data).digest('hex');
+    };
 
-describe('createHmacSubtle', () => {
-  it('should create an HMAC equivalent to node crypto createHmac', async () => {
-    const hmacSubtle = await createHmacSubtle('secret', 'data');
-    const hmacNode = createHmacNode('secret', 'data');
+    it('should create an HMAC equivalent to node crypto createHmac', async () => {
+      const hmacSubtle = await createHmacSubtle('secret', 'data');
+      const hmacNode = createHmacNode('secret', 'data');
 
-    expect(hmacSubtle).toEqual(hmacNode);
+      expect(hmacSubtle).toEqual(hmacNode);
+    });
   });
 });
