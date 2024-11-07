0112278df9c60ccb7e08299d88081300514fe5f1
Dima Grossman <dima@grossman.io>
2024-11-07 16:54:57 +0200

    refactor: decorated class

--- packages/framework/src/servers/nest/nest.utils.ts
+++ packages/framework/src/servers/nest/nest.utils.ts
@@ -1,8 +1,8 @@
 import { Type } from '@nestjs/common';
 
 export function applyDecorators<T>(baseClass: Type<T>, decorators: Array<ClassDecorator> = []): Type<T> {
-  return decorators.reduce((decoratedController, decorator) => {
-    const result = decorator(decoratedController);
+  return decorators.reduce((decoratedClass, decorator) => {
+    const result = decorator(decoratedClass);
 
     return result as Type<T>;
   }, baseClass);
