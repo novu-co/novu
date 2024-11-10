9feeba3fa73b1159ccd9664631233ca25c93044f
Pawan Jain <jainpawan211199@gmail.com>
2024-11-10 07:03:15 +0530

    feat(web): add plain script in index.html

--- apps/web/src/components/layout/components/v2/HeaderNav.tsx
+++ apps/web/src/components/layout/components/v2/HeaderNav.tsx
@@ -1,12 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ActionIcon, Header } from '@mantine/core';
-import { IconHelpOutline, IconOutlineChat, IconOutlineLibraryBooks, IconOutlineGroup } from '@novu/novui/icons';
+import {
+  IconHelpOutline,
+  IconOutlineChat,
+  IconOutlineLibraryBooks,
+  IconOutlineGroup,
+  IconOutlineMail,
+} from '@novu/novui/icons';
 import { Tooltip, Dropdown } from '@novu/design-system';
 import { css } from '@novu/novui/css';
 import { HStack } from '@novu/novui/jsx';
 import { FeatureFlagsKeysEnum } from '@novu/shared';
 import { IS_EE_AUTH_ENABLED, IS_NOVU_PROD_STAGING } from '../../../../config';
-import { useBootIntercom, useFeatureFlag } from '../../../../hooks';
+import { useFeatureFlag } from '../../../../hooks';
 import useThemeChange from '../../../../hooks/useThemeChange';
 import { discordInviteUrl } from '../../../../pages/quick-start/consts';
 import { useAuth } from '../../../../hooks/useAuth';
@@ -19,16 +25,55 @@ import { WorkflowHeaderBackButton } from './WorkflowHeaderBackButton';
 import { SupportModal } from '../SupportModal';
 
 export function HeaderNav() {
-  const { currentUser } = useAuth();
+  const { currentUser, currentOrganization } = useAuth();
   const [isSupportModalOpened, setIsSupportModalOpened] = useState(false);
   const isV2Enabled = useFeatureFlag(FeatureFlagsKeysEnum.IS_V2_ENABLED);
 
-  useBootIntercom();
+  const isLiveChatVisible =
+    process.env.REACT_APP_PLAIN_SUPPORT_CHAT_APP_ID &&
+    IS_NOVU_PROD_STAGING &&
+    currentOrganization?.apiServiceLevel !== 'free' &&
+    currentUser?.servicesHashes?.plain;
 
   const { Icon, themeLabel, toggleColorScheme } = useThemeChange();
 
   const toggleSupportModalShow = () => {
     setIsSupportModalOpened((previous) => !previous);
+  };
+
+  useEffect(() => {
+    if (isLiveChatVisible) {
+      // @ts-ignore
+      window.Plain.init({
+        appId: process.env.REACT_APP_PLAIN_SUPPORT_CHAT_APP_ID,
+        hideLauncher: true,
+        title: 'Chat with us',
+        links: [
+          {
+            icon: 'call',
+            text: 'Schedule a call with us',
+            url: 'https://notify.novu.co/meetings/novuhq/novu-discovery-session-rr?utm_campaign=in_app_live_chat',
+          },
+        ],
+        entryPoint: 'default',
+        theme: 'light',
+
+        customerDetails: {
+          email: currentUser?.email,
+          emailHash: currentUser?.servicesHashes?.plain,
+        },
+      });
+
+      // @ts-ignore
+      window.Plain.show();
+    }
+  }, [isLiveChatVisible, currentUser]);
+
+  const showLiveChat = () => {
+    if (currentUser?.servicesHashes?.plain && process.env.REACT_APP_PLAIN_SUPPORT_CHAT_APP_ID) {
+      // @ts-ignore
+      window.Plain.show();
+    }
   };
 
   return (
@@ -81,16 +126,26 @@ export function HeaderNav() {
                   </HStack>
                 </a>
               </Dropdown.Item>
-
               <Dropdown.Item
                 onClick={() => {
                   toggleSupportModalShow();
                 }}
               >
                 <HStack>
-                  <IconOutlineChat /> Contact Us
+                  <IconOutlineMail /> Contact Us
                 </HStack>
               </Dropdown.Item>
+              {isLiveChatVisible && (
+                <Dropdown.Item
+                  onClick={() => {
+                    showLiveChat();
+                  }}
+                >
+                  <HStack>
+                    <IconOutlineChat /> Live Chat
+                  </HStack>
+                </Dropdown.Item>
+              )}
             </Dropdown>
           ) : (
             <a href={discordInviteUrl} target="_blank" rel="noopener noreferrer">
