7fbd091ee642ffd92296f73fd6b62251485fb928
Adam Chmara <adam.chmara1@gmail.com>
2024-11-11 14:22:16 +0100

    feat(web,dashboard): opt-in/out tracking

--- apps/dashboard/src/hooks/use-new-dashboard-opt-in.ts
+++ apps/dashboard/src/hooks/use-new-dashboard-opt-in.ts
@@ -1,9 +1,12 @@
 import { LEGACY_DASHBOARD_URL } from '@/config';
+import { useTelemetry } from '@/hooks/use-telemetry';
+import { TelemetryEvent } from '@/utils/telemetry';
 import { useUser } from '@clerk/clerk-react';
 import { NewDashboardOptInStatusEnum } from '@novu/shared';
 
 export function useNewDashboardOptIn() {
   const { user } = useUser();
+  const track = useTelemetry();
 
   const updateUserOptInStatus = (status: NewDashboardOptInStatusEnum) => {
     if (!user) return;
@@ -28,21 +31,18 @@ export function useNewDashboardOptIn() {
   };
 
   const optIn = () => {
+    track(TelemetryEvent.NEW_DASHBOARD_OPT_IN);
     updateUserOptInStatus(NewDashboardOptInStatusEnum.OPTED_IN);
   };
 
   const optOut = () => {
+    track(TelemetryEvent.NEW_DASHBOARD_OPT_OUT);
     updateUserOptInStatus(NewDashboardOptInStatusEnum.OPTED_OUT);
-  };
-
-  const dismiss = () => {
-    updateUserOptInStatus(NewDashboardOptInStatusEnum.DISMISSED);
   };
 
   return {
     optIn,
     optOut,
-    dismiss,
     status: getCurrentOptInStatus(),
     redirectToLegacyDashboard,
   };
