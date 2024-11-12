c125ef0616db8e08230fa3645ccbb76c6fef7eea
Paweł <pawel.tymczuk@gmail.com>
2024-11-12 17:05:22 +0100

    feat(dashboard): page meta, gtm, and uptime

--- apps/dashboard/src/components/workflow-editor/steps/edit-step-sidebar.tsx
+++ apps/dashboard/src/components/workflow-editor/steps/edit-step-sidebar.tsx
@@ -6,6 +6,7 @@ import { Sheet, SheetOverlay, SheetPortal } from '@/components/primitives/sheet'
 import { useFetchWorkflow } from '@/hooks/use-fetch-workflow';
 import { StepEditor } from '@/components/workflow-editor/steps/step-editor';
 import { useFetchStep } from '@/hooks/use-fetch-step';
+import { PageMeta } from '@/components/page-meta';
 
 const transitionSetting = { ease: [0.29, 0.83, 0.57, 0.99], duration: 0.4 };
 
@@ -20,41 +21,44 @@ export const EditStepSidebar = () => {
   const stepType = useMemo(() => workflow?.steps.find((el) => el.slug === stepSlug)?.type, [stepSlug, workflow]);
 
   return (
-    <Sheet open>
-      <SheetPortal>
-        <SheetOverlay asChild>
+    <>
+      <PageMeta title={`Edit ${step?.name}`} />
+      <Sheet open>
+        <SheetPortal>
+          <SheetOverlay asChild>
+            <motion.div
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              exit={{
+                opacity: 0,
+              }}
+              transition={transitionSetting}
+            />
+          </SheetOverlay>
           <motion.div
             initial={{
-              opacity: 0,
+              x: '100%',
             }}
             animate={{
-              opacity: 1,
+              x: 0,
             }}
             exit={{
-              opacity: 0,
+              x: '100%',
             }}
             transition={transitionSetting}
-          />
-        </SheetOverlay>
-        <motion.div
-          initial={{
-            x: '100%',
-          }}
-          animate={{
-            x: 0,
-          }}
-          exit={{
-            x: '100%',
-          }}
-          transition={transitionSetting}
-          className={
-            'bg-background fixed inset-y-0 right-0 z-50 flex h-full w-3/4 flex-col border-l shadow-lg sm:max-w-[600px]'
-          }
-        >
-          {/* TODO: show loading indicator */}
-          {workflow && step && stepType && <StepEditor workflow={workflow} step={step} stepType={stepType} />}
-        </motion.div>
-      </SheetPortal>
-    </Sheet>
+            className={
+              'bg-background fixed inset-y-0 right-0 z-50 flex h-full w-3/4 flex-col border-l shadow-lg sm:max-w-[600px]'
+            }
+          >
+            {/* TODO: show loading indicator */}
+            {workflow && step && stepType && <StepEditor workflow={workflow} step={step} stepType={stepType} />}
+          </motion.div>
+        </SheetPortal>
+      </Sheet>
+    </>
   );
 };
