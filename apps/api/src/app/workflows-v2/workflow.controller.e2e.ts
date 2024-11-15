86adec43c363df7411d7eee770a025a4ceb54a71
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-11-16 00:08:25 +0100

    refactor(utils): move slugify to strings module

--- apps/api/src/app/workflows-v2/workflow.controller.e2e.ts
+++ apps/api/src/app/workflows-v2/workflow.controller.e2e.ts
@@ -407,7 +407,7 @@ describe('Workflow Controller E2E API Testing', () => {
 
       // Verify new created step
       expect(prodWorkflowUpdated.steps[1]._id).to.not.equal(prodWorkflowCreated.steps[1]._id);
-      expect(prodWorkflowUpdated.steps[1].stepId).to.equal('new-in-app-step');
+      expect(prodWorkflowUpdated.steps[1].stepId).to.equal('new-inapp-step');
     });
 
     it('should throw an error if trying to promote to the same environment', async () => {
