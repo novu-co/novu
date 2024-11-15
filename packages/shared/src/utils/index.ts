fac74164b3df7d3a15a972fdad1c524f3acb3cc1
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-11-15 23:57:39 +0100

    refactor(utils): move slugify to strings module

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -3,4 +3,4 @@ export * from './env';
 export * from './normalizeEmail';
 export * from './bridge.utils';
 export * from './buildWorkflowPreferences';
-export { slugify } from './slugify';
+export * from './strings';
