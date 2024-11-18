bd14332bf9927dd53030802c5693bfa323079b3a
Biswajeet Das <biswajeetdas18@gmail.com>
2024-11-18 13:21:02 +0530

    feat(dashboard): Enhance workflow status component with icons for better visual representation

--- apps/dashboard/src/components/workflow-status.tsx
+++ apps/dashboard/src/components/workflow-status.tsx
@@ -1,8 +1,8 @@
-import { Badge } from '@/components/primitives/badge';
-import { WorkflowStatusEnum } from '@/utils/enums';
 import { ComponentProps } from 'react';
-import { IconType } from 'react-icons/lib';
-import { RiAlertFill, RiCheckboxCircleFill, RiErrorWarningFill } from 'react-icons/ri';
+import { RiCheckboxCircleFill, RiErrorWarningFill, RiForbidFill } from 'react-icons/ri';
+import { Badge, BadgeContent } from '@/components/primitives/badge';
+import { WorkflowStatusEnum } from '@/utils/enums';
+import { type IconType } from 'react-icons/lib';
 
 type WorkflowStatusProps = {
   status: WorkflowStatusEnum;
@@ -12,12 +12,23 @@ const statusRenderData: Record<
   WorkflowStatusEnum,
   {
     badgeVariant: ComponentProps<typeof Badge>['variant'];
+    badgeContentVariant: ComponentProps<typeof BadgeContent>['variant'];
     text: string;
     icon: IconType;
   }
 > = {
-  [WorkflowStatusEnum.ACTIVE]: { badgeVariant: 'success', text: 'Active', icon: RiCheckboxCircleFill },
-  [WorkflowStatusEnum.INACTIVE]: { badgeVariant: 'warning', text: 'Inactive', icon: RiAlertFill },
+  [WorkflowStatusEnum.ACTIVE]: {
+    badgeVariant: 'success-light',
+    badgeContentVariant: 'success',
+    text: 'Active',
+    icon: RiCheckboxCircleFill,
+  },
+  [WorkflowStatusEnum.INACTIVE]: {
+    badgeVariant: 'soft',
+    badgeContentVariant: 'neutral',
+    text: 'Inactive',
+    icon: RiForbidFill,
+  },
   [WorkflowStatusEnum.ERROR]: {
     badgeVariant: 'destructive',
     text: 'Action required',
@@ -30,10 +41,12 @@ export const WorkflowStatus = (props: WorkflowStatusProps) => {
   const badgeVariant = statusRenderData[status].badgeVariant;
   const Icon = statusRenderData[status].icon;
   const text = statusRenderData[status].text;
-
+  const badgeContentVariant = statusRenderData[status].badgeContentVariant;
   return (
-    <Badge variant={badgeVariant}>
-      <Icon className="size-4" /> {text}
+    <Badge variant={badgeVariant} className="border-none py-1">
+      <BadgeContent variant={badgeContentVariant} className="flex w-max items-center gap-1">
+        <Icon className="size-4" /> {text}
+      </BadgeContent>
     </Badge>
   );
 };
