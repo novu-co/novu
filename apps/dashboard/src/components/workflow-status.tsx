c3ee94e92a15681dce0dafdd3232613b01518974
Biswajeet Das <biswajeetdas18@gmail.com>
2024-11-18 14:02:04 +0530

    feat(dashboard): Simplify workflow status component by removing BadgeContent and adjusting badge variants

--- apps/dashboard/src/components/workflow-status.tsx
+++ apps/dashboard/src/components/workflow-status.tsx
@@ -1,6 +1,6 @@
 import { ComponentProps } from 'react';
 import { RiCheckboxCircleFill, RiErrorWarningFill, RiForbidFill } from 'react-icons/ri';
-import { Badge, BadgeContent } from '@/components/primitives/badge';
+import { Badge } from '@/components/primitives/badge';
 import { WorkflowStatusEnum } from '@/utils/enums';
 import { type IconType } from 'react-icons/lib';
 
@@ -12,20 +12,17 @@ const statusRenderData: Record<
   WorkflowStatusEnum,
   {
     badgeVariant: ComponentProps<typeof Badge>['variant'];
-    badgeContentVariant: ComponentProps<typeof BadgeContent>['variant'];
     text: string;
     icon: IconType;
   }
 > = {
   [WorkflowStatusEnum.ACTIVE]: {
-    badgeVariant: 'success-light',
-    badgeContentVariant: 'success',
+    badgeVariant: 'success',
     text: 'Active',
     icon: RiCheckboxCircleFill,
   },
   [WorkflowStatusEnum.INACTIVE]: {
     badgeVariant: 'soft',
-    badgeContentVariant: 'neutral',
     text: 'Inactive',
     icon: RiForbidFill,
   },
@@ -41,12 +38,10 @@ export const WorkflowStatus = (props: WorkflowStatusProps) => {
   const badgeVariant = statusRenderData[status].badgeVariant;
   const Icon = statusRenderData[status].icon;
   const text = statusRenderData[status].text;
-  const badgeContentVariant = statusRenderData[status].badgeContentVariant;
+
   return (
     <Badge variant={badgeVariant} className="border-none py-1">
-      <BadgeContent variant={badgeContentVariant} className="flex w-max items-center gap-1">
-        <Icon className="size-4" /> {text}
-      </BadgeContent>
+      <Icon className="size-4" /> {text}
     </Badge>
   );
 };
