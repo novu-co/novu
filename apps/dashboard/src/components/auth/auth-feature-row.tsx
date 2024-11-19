94e6ba4acdc2a6c383b68cc81d11d3717681fd81
Dima Grossman <dima@grossman.io>
2024-11-19 19:20:22 +0200

    Update auth-feature-row.tsx

--- apps/dashboard/src/components/auth/auth-feature-row.tsx
+++ apps/dashboard/src/components/auth/auth-feature-row.tsx
@@ -8,11 +8,11 @@ interface AuthFeatureRowProps {
 
 export function AuthFeatureRow({ icon, title, description }: AuthFeatureRowProps) {
   return (
-    <div className="inline-flex items-start justify-start gap-3.5 self-stretch">
+    <div className="inline-flex items-start justify-start gap-3.5">
       <div className="flex items-center justify-center p-1">{icon}</div>
       <div className="inline-flex shrink grow basis-0 flex-col items-start justify-start gap-2">
-        <div className="self-stretch text-sm font-medium leading-tight text-[#0d111b]">{title}</div>
-        <div className="self-stretch text-xs font-medium text-[#99a0ad]">{description}</div>
+        <div className="text-sm font-medium text-[#0d111b]">{title}</div>
+        <div className="text-xs font-medium text-[#99a0ad]">{description}</div>
       </div>
     </div>
   );
