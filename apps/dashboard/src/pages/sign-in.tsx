b8fc292e6694bec0f797b487e6e7df49c45c1283
Dima Grossman <dima@grossman.io>
2024-11-19 18:53:20 +0200

    feat(dashboard): updated sign up page designs

--- apps/dashboard/src/pages/sign-in.tsx
+++ apps/dashboard/src/pages/sign-in.tsx
@@ -2,15 +2,19 @@ import { ROUTES } from '@/utils/routes';
 import { SignIn as SignInForm } from '@clerk/clerk-react';
 import { PageMeta } from '../components/page-meta';
 import { RegionPicker } from '../components/auth/region-picker';
+import { AuthSideBanner } from '../components/auth/auth-side-banner';
 import { clerkAppearance } from '@/utils/clerk-appearance';
 
 export const SignInPage = () => {
   return (
     <>
       <PageMeta title="Sign in" />
-      <div className="flex flex-col items-start justify-start gap-4">
-        <SignInForm path={ROUTES.SIGN_IN} signUpUrl={ROUTES.SIGN_UP} appearance={clerkAppearance} />
-        <RegionPicker />
+      <AuthSideBanner />
+      <div className="flex flex-1 items-center justify-end">
+        <div className="flex flex-col items-start justify-start gap-4">
+          <SignInForm path={ROUTES.SIGN_IN} signUpUrl={ROUTES.SIGN_UP} appearance={clerkAppearance} />
+          <RegionPicker />
+        </div>
       </div>
     </>
   );
--- apps/dashboard/src/pages/sign-up.tsx
+++ apps/dashboard/src/pages/sign-up.tsx
@@ -2,20 +2,24 @@ import { SignUp as SignUpForm } from '@clerk/clerk-react';
 import { PageMeta } from '@/components/page-meta';
 import { ROUTES } from '@/utils/routes';
 import { RegionPicker } from '@/components/auth/region-picker';
+import { AuthSideBanner } from '@/components/auth/auth-side-banner';
 import { clerkAppearance } from '@/utils/clerk-appearance';
 
 export const SignUpPage = () => {
   return (
     <>
       <PageMeta title="Sign up" />
-      <div className="flex flex-col items-start justify-start gap-[18px]">
-        <SignUpForm
-          path={ROUTES.SIGN_UP}
-          signInUrl={ROUTES.SIGN_IN}
-          appearance={clerkAppearance}
-          forceRedirectUrl={ROUTES.SIGNUP_ORGANIZATION_LIST}
-        />
-        <RegionPicker />
+      <AuthSideBanner />
+      <div className="flex flex-1 items-center justify-end">
+        <div className="flex flex-col items-start justify-start gap-[18px]">
+          <SignUpForm
+            path={ROUTES.SIGN_UP}
+            signInUrl={ROUTES.SIGN_IN}
+            appearance={clerkAppearance}
+            forceRedirectUrl={ROUTES.SIGNUP_ORGANIZATION_LIST}
+          />
+          <RegionPicker />
+        </div>
       </div>
     </>
   );
