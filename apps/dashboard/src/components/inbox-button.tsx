fc06a72050a2c05e444f9d19539c9af1d4adee9c
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-11-19 23:01:10 +0100

    feat(inbox-button): add jingle animation on notification

--- apps/dashboard/src/components/inbox-button.tsx
+++ apps/dashboard/src/components/inbox-button.tsx
@@ -8,12 +8,15 @@ import { useState, useEffect } from 'react';
 
 const InboxInner = () => {
   const [open, setOpen] = useState(false);
+  const [jingle, setJingle] = useState(false);
 
   const novu = useNovu();
   useEffect(() => {
     const cleanup = novu.on('notifications.notification_received', (data) => {
       console.log('notification_received');
       console.log({ data });
+      setJingle(true);
+      setTimeout(() => setJingle(false), 3000);
     });
     return () => cleanup();
   }, [novu]);
@@ -25,7 +28,9 @@ const InboxInner = () => {
           renderBell={(unreadCount) => (
             <HeaderButton label={unreadCount ? `Inbox (${unreadCount})` : 'Inbox'} disableTooltip={open}>
               <div className="relative flex items-center justify-center">
-                <InboxBellFilled className="text-foreground-600 size-4 cursor-pointer stroke-[0.5px]" />
+                <InboxBellFilled
+                  className={`text-foreground-600 size-4 cursor-pointer stroke-[0.5px] ${jingle ? 'animate-jingle' : ''}`}
+                />
                 {unreadCount > 0 && (
                   <div className="absolute right-[-4px] top-[-6px] flex h-3 w-3 items-center justify-center rounded-full border-[3px] border-[white] bg-white">
                     <span className="bg-destructive block h-1.5 w-1.5 animate-[pulse-shadow_1s_ease-in-out_infinite] rounded-full [--pulse-color:var(--destructive)]"></span>
