5a415ce1698f773d2f8642394c959c14b52c2931
Dima Grossman <dima@grossman.io>
2024-11-26 11:50:30 +0200

    Update organization-list.tsx

--- apps/dashboard/src/pages/organization-list.tsx
+++ apps/dashboard/src/pages/organization-list.tsx
@@ -1,5 +1,4 @@
 import { PageMeta } from '@/components/page-meta';
-import { AuthCard } from '../components/auth/auth-card';
 import OrganizationCreate from '../components/auth/create-organization';
 
 export const OrganizationListPage = () => {
