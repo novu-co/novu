9bf3bf48e09f86dd511f7e8bc49112104e2cb778
Dima Grossman <dima@grossman.io>
2024-11-28 16:47:24 +0200

    Update inbox-embed.tsx

--- apps/dashboard/src/components/welcome/inbox-embed.tsx
+++ apps/dashboard/src/components/welcome/inbox-embed.tsx
@@ -432,30 +432,6 @@ export function InboxEmbed(): JSX.Element {
                     </Button>
                   </div>
                 </div>
-
-                <div className="w-full max-w-[500px]">
-                  <CodeBlock
-                    secretMask={[{ line: 2, maskStart: 26, maskEnd: 37 }]}
-                    code={`curl -X POST 'https://api.novu.co/v1/events/trigger' \
-
--H 'Authorization: ApiKey NOVU_SECRET_KEY' \
-
--H 'Content-Type: application/json' \
-
--d '{
-  "name": "onboarding_workflow",
-  "to": {
-    "subscriberId": "657c929208be7e008a458508"
-  },
-  "payload": {
-    "userName": "Naveen",
-    "organizationProfile": "https://lh3.googleusercontent.com/a/AEdFTp5OW-iWTevL6YlYHpaZ6z-D0pVXE0V_7Z9Dgp12=s100",
-  }
-}'`}
-                    language="shell"
-                    title="Terminal"
-                  />
-                </div>
               </div>
             </div>
           </div>
