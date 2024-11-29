612e0cd23f775b757daab4763743886046d5cf94
Dima Grossman <dima@grossman.io>
2024-11-29 13:24:54 +0200

    Update framework-guides.tsx

--- apps/dashboard/src/components/welcome/framework-guides.tsx
+++ apps/dashboard/src/components/welcome/framework-guides.tsx
@@ -248,8 +248,37 @@ export function YourCustomInbox() {
       {
         title: 'Add the inbox code to your JavaScript app',
         description: 'Implement the notification center in your vanilla JavaScript application.',
-        code: 'npm install @novu/notification-center',
-        codeLanguage: 'shell',
+        code: `import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NovuUI } from '@novu/js/ui';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild('notificationInbox') notificationInbox!: ElementRef<HTMLElement>;
+  title = 'inbox-angular';
+
+  ngAfterViewInit() {
+    const novu = new NovuUI({
+      options: {
+        applicationIdentifier: '123',
+        subscriberId: '456',
+      },
+    });
+
+    novu.mountComponent({
+      name: 'Inbox',
+      props: {},
+      element: this.notificationInbox.nativeElement,
+    });
+  }
+}`,
+        codeLanguage: 'typescript',
       },
     ],
   },
