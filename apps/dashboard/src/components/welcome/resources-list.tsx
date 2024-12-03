f4ae372dc47bf028df8c762a70aadc1c36a99f54
Dima Grossman <dima@grossman.io>
2024-12-03 18:13:02 +0200

    fix: border

--- apps/dashboard/src/components/welcome/resources-list.tsx
+++ apps/dashboard/src/components/welcome/resources-list.tsx
@@ -74,7 +74,7 @@ export function ResourcesList({ resources, title, icon }: ResourcesListProps) {
           {resources.map((resource, index) => (
             <motion.div key={index} variants={itemVariants}>
               <Link to={resource.url} target="_blank" rel="noopener" onClick={() => handleResourceClick(resource)}>
-                <Card className="w-60 shrink-0 overflow-hidden border-none shadow-[0px_12px_32px_0px_rgba(0,0,0,0.02),0px_0px_0px_1px_rgba(0,0,0,0.05)] transition-all duration-200 hover:translate-y-[1px] hover:cursor-pointer hover:shadow-md">
+                <Card className="w-60 shrink-0 overflow-hidden border-none shadow-[0px_12px_32px_0px_rgba(0,0,0,0.02),0px_0px_0px_1px_rgba(0,0,0,0.05)] transition-all duration-200">
                   <motion.div
                     className="bg-foreground-50 h-[126px] overflow-hidden"
                     whileHover={{ scale: 1.03 }}
