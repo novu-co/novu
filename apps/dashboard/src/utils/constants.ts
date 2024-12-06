3f9212664936efa9305b224521bceeee1ceee81f
Adam Chmara <adam.chmara1@gmail.com>
2024-12-06 12:55:28 +0100

    feat: add ff

--- apps/dashboard/src/utils/constants.ts
+++ apps/dashboard/src/utils/constants.ts
@@ -6,12 +6,6 @@ export const AUTOCOMPLETE_PASSWORD_MANAGERS_OFF = {
   'data-form-type': 'other',
 };
 
-export const UNSUPPORTED_STEP_TYPES: readonly StepTypeEnum[] = [
-  StepTypeEnum.DIGEST,
-  StepTypeEnum.TRIGGER,
-  StepTypeEnum.CUSTOM,
-];
-
 export const INLINE_CONFIGURABLE_STEP_TYPES: readonly StepTypeEnum[] = [StepTypeEnum.DELAY];
 
 export const TEMPLATE_CONFIGURABLE_STEP_TYPES: readonly StepTypeEnum[] = [
