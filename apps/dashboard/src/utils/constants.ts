4c634b8bc1572c8d8b336088b3c3ceadc3fafb93
Adam Chmara <adam.chmara1@gmail.com>
2024-12-06 12:39:57 +0100

    fix(dashboard): configurable code-first workflows

--- apps/dashboard/src/utils/constants.ts
+++ apps/dashboard/src/utils/constants.ts
@@ -14,4 +14,10 @@ export const UNSUPPORTED_STEP_TYPES: readonly StepTypeEnum[] = [
 
 export const INLINE_CONFIGURABLE_STEP_TYPES: readonly StepTypeEnum[] = [StepTypeEnum.DELAY];
 
-export const TEMPLATE_CONFIGURABLE_STEP_TYPES: readonly StepTypeEnum[] = [StepTypeEnum.IN_APP, StepTypeEnum.EMAIL];
+export const TEMPLATE_CONFIGURABLE_STEP_TYPES: readonly StepTypeEnum[] = [
+  StepTypeEnum.IN_APP,
+  StepTypeEnum.EMAIL,
+  StepTypeEnum.SMS,
+  StepTypeEnum.CHAT,
+  StepTypeEnum.PUSH,
+];
