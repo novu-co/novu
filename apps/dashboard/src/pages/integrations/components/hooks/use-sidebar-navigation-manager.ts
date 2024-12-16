3e60fe8c7eab6978069591dfa67104d713c1d347
Dima Grossman <dima@grossman.io>
2024-12-16 13:09:32 +0000

    fix: integration step import

--- apps/dashboard/src/pages/integrations/components/hooks/use-sidebar-navigation-manager.ts
+++ apps/dashboard/src/pages/integrations/components/hooks/use-sidebar-navigation-manager.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { IntegrationStep } from '../types';
+import { IntegrationStep } from '../../types';
 
 interface UseSidebarNavigationManagerProps {
   isOpened: boolean;
