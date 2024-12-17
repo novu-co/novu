be4f68a1b7251817a4a13a7a387ee6c2c48dd67a
Biswajeet Das <biswajeetdas18@gmail.com>
2024-12-17 15:41:45 +0530

    fix(dashboard): implement redirect to legacy studio auth page

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -30,6 +30,7 @@ import { ChannelPreferences } from './components/workflow-editor/channel-prefere
 import { FeatureFlagsProvider } from './context/feature-flags-provider';
 import { ConfigureStep } from '@/components/workflow-editor/steps/configure-step';
 import { ConfigureStepTemplate } from '@/components/workflow-editor/steps/configure-step-template';
+import { RedirectToLegacyStudioAuth } from './pages/redirect-to-legacy-studio-auth';
 
 initializeSentry();
 overrideZodErrorMap();
@@ -159,12 +160,7 @@ const router = createBrowserRouter([
           },
           {
             path: ROUTES.LOCAL_STUDIO_AUTH,
-            element: (() => {
-              const currentSearch = window.location.search;
-              window.location.href = `${LEGACY_ROUTES.LOCAL_STUDIO_AUTH}${currentSearch}&studio_path_hint=/legacy/studio`;
-
-              return null;
-            })(),
+            element: <RedirectToLegacyStudioAuth />,
           },
           {
             path: '*',
