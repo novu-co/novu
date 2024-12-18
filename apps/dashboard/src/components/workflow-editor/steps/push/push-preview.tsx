5281fa9d33f82f2a9c170171959400c37775d96a
Biswajeet Das <biswajeetdas18@gmail.com>
2024-12-18 15:17:37 +0530

    feat(dashboard): update push background image to iPhone version

--- apps/dashboard/src/components/workflow-editor/steps/push/push-preview.tsx
+++ apps/dashboard/src/components/workflow-editor/steps/push/push-preview.tsx
@@ -111,7 +111,10 @@ export const PushContentContainerPreview = ({ children, className, ...rest }: HT
 
 export const PushBackgroundWithPhone = ({ children, className, ...rest }: HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className={cn("relative h-60 w-full max-w-72 bg-[url('/images/push-bg.svg')] bg-cover", className)} {...rest}>
+    <div
+      className={cn("relative h-60 w-full max-w-72 bg-[url('/images/phones/iphone-push.svg')] bg-cover", className)}
+      {...rest}
+    >
       {children}
     </div>
   );
