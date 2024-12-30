f97c433865b8e31227129a082d1bd0a80dd1ecec
Dima Grossman <dima@grossman.io>
2024-12-30 21:03:14 +0200

    fix: remove unused

--- apps/dashboard/src/components/primitives/field-editor/variable-plugin/constants.ts
+++ apps/dashboard/src/components/primitives/field-editor/variable-plugin/constants.ts
@@ -1,4 +1,3 @@
 export const VARIABLE_REGEX = /{{([^{}]+)}}/g;
-export const DARK_MODE_CLASS = 'dark';
 export const VARIABLE_PILL_CLASS = 'cm-variable-pill';
 export const MODIFIERS_CLASS = 'has-modifiers';
