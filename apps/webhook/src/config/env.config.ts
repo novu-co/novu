f67fb9ecbcf52a7c188f3839848110ef88b67599
Himanshu Garg <garg_himanshu@outlook.com>
2025-01-02 14:44:41 +0530

    fix(env): update environment variable reference from NOVU_ENV to NODE_ENV in Dockerfiles and config files

--- apps/webhook/src/config/env.config.ts
+++ apps/webhook/src/config/env.config.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 import path from 'node:path';
 import { getContextPath, NovuComponentEnum, getEnvFileNameForNodeEnv } from '@novu/shared';
 
-dotenv.config({ path: path.join(__dirname, '..', getEnvFileNameForNodeEnv(process.env.NOVU_ENV)) });
+dotenv.config({ path: path.join(__dirname, '..', getEnvFileNameForNodeEnv(process.env.NODE_ENV)) });
 
 export const CONTEXT_PATH = getContextPath(NovuComponentEnum.WEBHOOK);
