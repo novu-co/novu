220c396b315d113e5e1655eb92762c0d6cc19bf2
Sokratis Vidros <sokratis.vidros@gmail.com>
2025-01-09 00:28:13 +0200

    chore(testing): Move ClerkClientMock there
    Move ClerkClientMock to testing from ee packages to be consumed as a universal test utility.

--- apps/api/e2e/setup.ts
+++ apps/api/e2e/setup.ts
@@ -1,27 +1,10 @@
-import { DalService } from '@novu/dal';
 import { testServer } from '@novu/testing';
 import sinon from 'sinon';
 import chai from 'chai';
 
 import { bootstrap } from '../src/bootstrap';
-import { isClerkEnabled } from '@novu/shared';
 
-const dalService = new DalService();
-
-console.log('Dummy change');
-
-async function seedClerkMongo() {
-  if (isClerkEnabled()) {
-    const clerkClientMock = require('@novu/ee-auth')?.ClerkClientMock;
-
-    if (clerkClientMock) {
-      const clerkClient = new clerkClientMock();
-      await clerkClient.seedDatabase();
-    } else {
-      throw new Error('ClerkClientMock not found');
-    }
-  }
-}
+console.log('Testing e2e...');
 
 before(async () => {
   /**
@@ -29,21 +12,10 @@ before(async () => {
    */
   chai.config.truncateThreshold = 0;
   await testServer.create(await bootstrap());
-
-  await dalService.connect(process.env.MONGO_URL);
-  await seedClerkMongo();
 });
 
 after(async () => {
   await testServer.teardown();
-
-  try {
-    await dalService.destroy();
-  } catch (e) {
-    if (e.code !== 12586) {
-      throw e;
-    }
-  }
 });
 
 afterEach(() => {
