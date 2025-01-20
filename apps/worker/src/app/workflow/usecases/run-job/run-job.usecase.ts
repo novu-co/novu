5a2af724974cb26b09b9c4f2dd844eb04087c84b
Himanshu Garg <garg_himanshu@outlook.com>
2025-01-20 15:13:25 +0530

    fix(worker): change error logging to standard log for job execution errors

--- apps/worker/src/app/workflow/usecases/run-job/run-job.usecase.ts
+++ apps/worker/src/app/workflow/usecases/run-job/run-job.usecase.ts
@@ -107,7 +107,7 @@ export class RunJob {
         await this.jobRepository.updateStatus(job._environmentId, job._id, JobStatusEnum.COMPLETED);
       }
     } catch (error: any) {
-      Logger.error({ error }, `Running job ${job._id} has thrown an error`, LOG_CONTEXT);
+      Logger.log({ error }, `Running job ${job._id} has thrown an error`, LOG_CONTEXT);
       if (job.step.shouldStopOnFail || this.shouldBackoff(error)) {
         shouldQueueNextJob = false;
       }
